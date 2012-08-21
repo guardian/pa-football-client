37c8fd833f41f75289328772d92c3f5ea6aafe1d
Grant Klopper <grant.klopper@guardian.co.uk>
2012-08-21 17:06:13 +0100

    Moved repeated code into a method.

--- src/main/scala/pa/PaClient.scala
+++ src/main/scala/pa/PaClient.scala
@@ -10,28 +10,27 @@ trait PaClient { self: Http =>
 
   lazy val base: String = "http://pads2.pa-sport.com"
 
-  def competitions: Seq[Season] = GET(base + "/api/football/competitions/%s/json".format(apiKey)) match {
-    case Response(200, body, _) =>  parseCompetitions(body)
+  def competitions: Seq[Season] = get("/api/football/competitions/%s/json".format(apiKey)){
+    parseCompetitions
+  }
+
+  def matchEvents(id: String): MatchEvents = get("/api/football/match/events/%s/%s/json".format(apiKey, id)) {
+    parseMatchEvents
+  }
+
+  def matchStats(id: String): MatchStats = get("/api/football/match/stats/%s/%s/json".format(apiKey, id)) {
+    parseMatchStats
+  }
+
+  def matchDay(competitionId: String, date: DateMidnight): Seq[MatchDay] =
+    get("/api/football/competition/matchDay/%s/%s/%s/json".format(apiKey, competitionId, date.toString("yyyyMMdd"))) {
+      parseMatchDay
+  }
+
+  private def get[T](path: String)(parser: String => T) = GET(base + path) match {
+    case Response(200, body, _) => parser(body)
     case Response(status, _, reason) => throw new PaClientException(status + " " + reason)
   }
-
-  def matchEvents(id: String): MatchEvents = GET(base + "/api/football/match/events/%s/%s/json".format(apiKey, id)) match {
-    case Response(200, body, _) =>  parseMatchEvents(body)
-    case Response(status, _, reason) => throw new PaClientException(status + " " + reason)
-  }
-
-  def matchStats(id: String): MatchStats = GET(base + "/api/football/match/stats/%s/%s/json".format(apiKey, id)) match {
-    case Response(200, body, _) =>  parseMatchStats(body)
-    case Response(status, _, reason) => throw new PaClientException(status + " " + reason)
-  }
-
-  def matchDay(competitionId: String, date: DateMidnight): Seq[MatchDay] =GET(
-    base + "/api/football/competition/matchDay/%s/%s/%s/json".format(apiKey, competitionId, date.toString("yyyyMMdd"))
-  ) match {
-      case Response(200, body, _) =>  parseMatchDay(body)
-      case Response(status, _, reason) => throw new PaClientException(status + " " + reason)
-  }
-
 }
 
 class PaClientException(msg: String) extends RuntimeException(msg)
