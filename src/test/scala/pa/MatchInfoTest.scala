536d5c0c4f2d4be44929f42b9470adbb66fa4579
Robert Berry <robert.berry@guardian.co.uk>
2014-02-20 11:31:32 +0000

    Remove noise from match info test

--- src/test/scala/pa/MatchInfoTest.scala
+++ src/test/scala/pa/MatchInfoTest.scala
@@ -10,37 +10,38 @@ class MatchInfoTest extends FlatSpec with ShouldMatchers {
   "PaClient" should "load matchInfo" in {
     val theMatch = Await.result(StubClient.matchInfo("3695171"), 1.second)
 
-    theMatch.id should be("3695171")
-    theMatch.round should be(Some(Round("7", "Semi-Final Second Leg")))
-    theMatch.leg should be("2")
-    theMatch.liveMatch should be(false)
-    theMatch.result should be(true)
-    theMatch.previewAvailable should be(true)
-    theMatch.reportAvailable should be(true)
-    theMatch.lineupsAvailable should be(true)
-    theMatch.matchStatus should be("FT")
-    theMatch.attendance should be(None)
-    theMatch.referee should be(Some(Official("182780", "Lee Mason")))
-    theMatch.venue should be(Some(Venue("114", "Old Trafford")))
+    theMatch should have (
+      'id ("3695171"),
+      'round (Some(Round("7", "Semi-Final Second Leg"))),
+      'leg ("2"),
+      'liveMatch (false),
+      'result (true),
+      'previewAvailable (true),
+      'reportAvailable (true),
+      'lineupsAvailable (true),
+      'matchStatus ("FT"),
+      'attendance (None),
+      'referee (Some(Official("182780", "Lee Mason"))),
+      'venue (Some(Venue("114", "Old Trafford"))),
+      'comments (Some("(Sunderland win 2-1 on penalties)"))
+    )
 
-    val homeTeam = theMatch.homeTeam
+    theMatch.homeTeam should have (
+      'id ("12"),
+      'name ("Man Utd"),
+      'score (Some(2)),
+      'htScore (Some(1)),
+      'aggregateScore (Some(3)),
+      'scorers (Some("Jonny Evans (37),Javier Hernandez (120 +0:28)"))
+    )
 
-    homeTeam.id should be("12")
-    homeTeam.name should be("Man Utd")
-    homeTeam.score should be(Some(2))
-    homeTeam.htScore should be(Some(1))
-    homeTeam.aggregateScore should be(Some(3))
-    homeTeam.scorers should be(Some("Jonny Evans (37),Javier Hernandez (120 +0:28)"))
-
-    val awayTeam = theMatch.awayTeam
-
-    awayTeam.id should be("39")
-    awayTeam.name should be("Sunderland")
-    awayTeam.score should be(Some(1))
-    awayTeam.htScore should be(Some(0))
-    awayTeam.aggregateScore should be(Some(3))
-    awayTeam.scorers should be(Some("Phil Bardsley (119)"))
-
-    theMatch.comments should be(Some("(Sunderland win 2-1 on penalties)"))
+    theMatch.awayTeam should have (
+      'id ("39"),
+      'name ("Sunderland"),
+      'score (Some(1)),
+      'htScore (Some(0)),
+      'aggregateScore (Some(3)),
+      'scorers (Some("Phil Bardsley (119)"))
+    )
   }
 }
